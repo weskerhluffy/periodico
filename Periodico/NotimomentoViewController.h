89f49baa736e9a6bbf712dfcec3e8f50057866a0
Ernesto Alvarado <ernesto@white-queen.gateway.2wire.net>
2011-10-30 14:43:00 -0600

    Se crean los view controlares de portada y notimomento vacíos.

---
+++ Periodico/NotimomentoViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotimomentoViewController.h
+//  Periodico
+//
+//  Created by Ernesto Alvarado on 30/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotimomentoViewController : UIViewController
+
+@end
